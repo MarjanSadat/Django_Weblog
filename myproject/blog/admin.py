694ce48b36513c27bd172eed9d153dfaf8b27de1
MarjanSadat <marjanmousavi@ymail.com>
2024-10-14 22:02:05 +0330

    first commit

---
+++ myproject/blog/admin.py
@@ -0,0 +1,65 @@
+from django.contrib import admin, messages
+from .models import Article, Category
+from django.utils.translation import ngettext
+
+#admin header change
+
+admin.site.site_header = 'مدیریت وبلاگ'
+
+# Register your models here.
+
+# @admin.action(description='انتشار مقالات انتخاب شده')
+# def make_published(modeladmin, request, queryset):
+# 	queryset.update(status='p')
+
+
+@admin.action(description='انتشار مقالات انتخاب شده')
+def make_published(modeladmin, request, queryset):
+    updated = queryset.update(status="p")
+    modeladmin.message_user(
+        request,
+        ngettext(
+            "%d مقاله به درستی منتشر شد.",
+            "%d مقالات به درستی منتشر شد.",
+            updated,
+        )
+        % updated,
+        messages.SUCCESS,
+    )
+
+
+
+@admin.action(description='پیش نویس شدن مقالات انتخاب شده')
+def make_draft(modeladmin, request, queryset):
+	updated = queryset.update(status="d")
+	modeladmin.message_user(
+		request,
+		ngettext(
+			"%d مقاله به درستی پیس نویس شد.",
+			"%d مقالات به درستی پیس نویس شد.",
+			updated,
+		)
+		% updated,
+		messages.SUCCESS,
+	)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ('position','title','slug','parent','status')
+	list_filter = (['status'])
+	search_fields = ('title','slug')
+	prepopulated_fields = {'slug' : ('title',)}
+
+admin.site.register(Category, CategoryAdmin)
+
+class ArticleAdmin(admin.ModelAdmin):
+	list_display = ('title','thumbnail_tag','slug','jpublish','status', 'category_to_str')
+	list_filter = ('publish','status')
+	search_fields = ('title','description')
+	prepopulated_fields = {'slug' : ('title',)}
+	ordering = ['status','publish'] #['-publish'] descending
+	actions = [make_published, make_draft]
+	def category_to_str(self, obj):
+		return ','.join([category.title for category in obj.category_published()])
+
+admin.site.register(Article, ArticleAdmin)
---
+++ myproject/blog/views.py
@@ -0,0 +1,59 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+# from django.http import HttpResponse, JsonResponse, Http404
+
+from .models import Article, Category
+
+from django.views.generic import ListView
+
+# Create your views here.
+
+def home(request, page=1):
+	
+	articles_list = Article.objects.published()
+	paginator = Paginator(articles_list, 3)
+	# page = request.GET.get('page')
+	articles = paginator.get_page(page)
+
+	context = {
+		# 'articles' : Article.objects.all()
+		# 'articles'  : Article.objects.filter(status='p').order_by('-publish')[:3]
+		# 'articles' : Article.objects.filter(status = 'p'),
+
+		'articles' : articles,
+		'categories' : Category.objects.filter(status = True)
+	}
+	return render(request,'blog/home.html',context)
+
+
+def detail(request,slug):
+	
+	# try:
+	# 	article = Article.objects.get(slug=slug)
+	# except Exception as e:
+	# 	raise Http404
+	
+	context = {
+		# 'article' : get_object_or_404(Article, slug=slug, status='p'),
+		'article' : get_object_or_404(Article.objects.published(), slug=slug, status='p'),
+
+		'categories' : Category.objects.filter(status = True)
+
+
+	}
+
+	return render(request, 'blog/detail.html', context)
+
+def category(request, slug, page=1):
+	
+	category = get_object_or_404(Category, slug=slug, status = True)
+	articles_list = category.articles.published()
+	paginator = Paginator(articles_list, 3)
+	articles = paginator.get_page(page)
+
+	context = {
+		'category' : category,
+		'articles' : articles
+	}
+
+	return render(request, 'blog/category.html', context)
