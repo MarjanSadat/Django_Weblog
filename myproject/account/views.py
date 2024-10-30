69f3e5b39f57bb8524ed71a1304904fc034148e2
MarjanSadat <marjanmousavi@ymail.com>
2024-10-31 00:37:57 +0330

    add change password in profile

--- myproject/account/views.py
+++ myproject/account/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordChangeView
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from blog.models import Article
 from .models import User
@@ -64,3 +64,6 @@ class Login(LoginView):
 			return reverse_lazy('account:home')
 		else:
 			return reverse_lazy('account:profile')
+
+class PasswordChange(PasswordChangeView):
+	success_url = reverse_lazy('account:password_change_done')
